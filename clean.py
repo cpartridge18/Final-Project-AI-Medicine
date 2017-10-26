9b182326cdc7b161c34489caa5d7c4440d6adb24
carsonprindle <carson_prindle18@milton.edu>
2017-10-26 13:32:59 -0400

    Added comments and comparison test

--- clean.py
+++ clean.py
@@ -5,7 +5,7 @@ import pandas as pd
 train = pd.read_csv("labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
 
 #relic of the movie review thing, rev is the number review to read
-rev = 70
+rev = 32
 
 # Import BeautifulSoup
 from bs4 import BeautifulSoup             
@@ -14,40 +14,34 @@ import re
 # Initialize the BeautifulSoup object on a single movie review     
 example1 = BeautifulSoup(train["review"][rev], "html5lib")  
 
-# Use regular expressions to do a find-and-replace
-letters_only = re.sub("[^a-zA-Z]",           # The pattern to search for
-                      " ",                   # The pattern to replace it with
-                      example1.get_text() )  # The text to search
-print(letters_only)
+# Method to give a list of all the words in the comparison string
+def just_words(input):
+	letters_only = re.sub("[^a-zA-Z]",   # The pattern to search for
+                      " ",               # The pattern to replace it with
+                      input)             # The text to search
+	lower_case = letters_only.lower()    # Convert to lower case
+	words = lower_case.split()      # Split into words
+	return words
 
-lower_case = letters_only.lower()        # Convert to lower case
-words = lower_case.split()               # Split into words
-print(words)
+# Just the words from the input
+in_words = just_words(example1.get_text())
+#print(words)
 
-#get stopwords
+# Get stopwords
 import nltk
 from nltk.corpus import stopwords # Import the stop word list
-print(str(stopwords.words("english")))
+#print(str(stopwords.words("english")))
 
-#create new list of words without stopwords using list comprehension
-trim = [w for w in words if w not in set(stopwords.words("english"))]
-print (trim)
+# Create new list of input words without stopwords using list comprehension
+trim = [w for w in in_words if w not in set(stopwords.words("english"))]
+#print (trim)
 
-#add something to count the number of duplicates
+# Import collections to count the number of duplicates
 import collections
 count = collections.Counter(trim)
-print (count)
+#print (count)
 
-#List of all the words in the comparison string
-comp_input = "" #comparison string
-comp_letters_only = re.sub("[^a-zA-Z]",      # The pattern to search for
-                      " ",                   # The pattern to replace it with
-                      comp_input)            # The text to search
-comp_lower_case = comp_letters_only.lower()        # Convert to lower case
-comp_words = comp_lower_case.split()               # Split into words
-print(comp_words)
-
-#method taking two input lists giving a list of the first list and a 1 or 0 depending on if the second word appears
+# Method taking two input lists giving a dictionary of the first list and a 1 or 0 depending on if the word appears in the second list
 def match(list, comp_list):
     D = {}
     for w in trim:
@@ -57,12 +51,20 @@ def match(list, comp_list):
             D[w]=0
 
     return(D)
-print(match(trim, comp_words))
+
+#Comparison Text
+comp = "faithful adaptation, humerous, this is the best, down with the bourgeois"
+comp_words = just_words(comp)
+
+# Dictionary with input compared to comparison list with 1's and 0's
+comp = match(trim, comp_words)
+print(comp)
 
 #print just the 1's and 0's
-print(list(match(trim, comp_words).values()))
+print(list(comp.values()))
 
+#Counter of 
 counter=collections.Counter(list(match(trim, comp_words).values()))
 print(counter.values())
 sim = float(counter[1]/(counter[0]+counter[1]))   #percent of words that appeared in the compared text
-print(sim)
+print("Percent Similarity: " + str(sim))
